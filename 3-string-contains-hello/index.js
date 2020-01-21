906c7f9bd0b3b015efd6151548a80971962938db
jennifer--MM <jenlynmenard.com>
2020-01-20 21:14:14 -0500

    finished string

--- 3-string-contains-hello/index.js
+++ 3-string-contains-hello/index.js
@@ -8,19 +8,21 @@ let input = getargs.getStringArg()
 
 /**** Do not modify anything above this line ****/
 
-// TODO make sure that the user entered a string
-if () {
-  //TODO use console.log to add a usage statement and let the user know that if a string contains a space it must be quoted
-} 
+
+if (!input) {
+  console.log("usage: node 3-string-contains-hello <sting>");
+  console.log("to include spaces, use quote.");
+}
 
 // check to see if string contains hello
 // if it does report back the position in the string where it begins
-
 else {
   // TODO write a logical expression to test if the input contains a "hello"
-  if () {
+  if (input.indexOf('hello') > -1) {
+    console.log(`hello was found at position ${input.indexOf(`hello`)} of the string ${input}.`);
     // TODO use console.log and a template literal at what position the hello was found
   } else {
-    // TODO use console.log to indicate that the hello was not found
+    console.log(`hello was not found in the ${input}.`);
+    // TODO use console.log to indicate that the hello was not found ${input}
   }
 }
